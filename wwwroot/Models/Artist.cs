4d3cf8a83b0bda4ebf2db1fcc006d93dac9278eb
Beatrice1404 <lazarbeatrice51@yahoo.com>
2024-07-08 17:27:24 +0300

    Add project files.

---
+++ wwwroot/Models/Artist.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Artist
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+    public List<Album> Albums { get; set; } = null!;
+}
+
+public class Album
+{
+    public string Title { get; set; } = null!;
+    public List<Song> Songs { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
+
+public class Song
+{
+    public string Title { get; set; } = null!;
+    public string Length { get; set; } = null!;
+}
